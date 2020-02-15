3f624a6d24d0a26aab4daf3e6bda8e1685f21328
Gabriel TCM <gabrieltcm@Gabriels-MacBook-Pro.local>
2020-02-15 17:13:25 +0800

    Updates

---
+++ precache-manifest.5512f002b9f2cd46be32ada33e61f31c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80b76b4684baae45a5c383b8c0ca61c",
+    "url": "/todo-lists/index.html"
+  },
+  {
+    "revision": "e95f2f86cab04b029fef",
+    "url": "/todo-lists/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "ab21bc0658b2e04c85a7",
+    "url": "/todo-lists/static/js/2.5184d29a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/todo-lists/static/js/2.5184d29a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95f2f86cab04b029fef",
+    "url": "/todo-lists/static/js/main.2070e7ed.chunk.js"
+  },
+  {
+    "revision": "a8e5485fa0c9a1bf07af",
+    "url": "/todo-lists/static/js/runtime-main.1c71e4cf.js"
+  }
+]);
